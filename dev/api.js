93a3e2e81cbe76d466a1d043951ed357531da517
unknown <talvindersingh.ca@gmail.com>
2018-10-28 12:18:19 -0400

    Added body-parser, basic impl for /transaction route-api

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 var app = express();
 
-// Starting endpoint '/'.
-app.get('/', function(req, res) {
-    res.send('My App for Blockchain')
+
+// Allow the json data or form data, we should be able to
+// access data through bosy parser.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Get Blockchain.
+app.get('/blockchain', function(req, res) {
+    
+});
+
+// Post transaction.
+app.post('/transaction', function(req, res) {
+    console.log(req.body);
+    res.send(`The amount of Transaction is ${req.body.amount} bitcoin`);
+});
+
+// Mine blockchain.
+app.get('/mine', function(req, res) {
+    
 });
 
 // listen to port 3000.
-app.listen(3000);
-console.log('My Blockchain app started at port 3000') ;
+app.listen(3000, function() {
+    console.log('My Blockchain app started at port 3000');
+});
